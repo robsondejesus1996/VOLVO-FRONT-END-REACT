45c0766c49c5d62a49464f0631b971635349cbba
Robson de Jesus <robsondejesus1996@hotmail.com>
2024-07-02 20:47:53 -0300

    feat: create hooks useCars

--- src/components/HelloWorld.tsx
+++ src/components/HelloWorld.tsx
@@ -1,7 +1,9 @@
 import React from "react";
 import { Block, Button } from "vcc-ui";
+import { useCars } from "./hooks/useCars";
 
 export const HelloWorld: React.FC = () => {
+  const { cars } = useCars();
   return (
     <Block extend={{ padding: 20 }}>
       <Button>Click me!</Button>
